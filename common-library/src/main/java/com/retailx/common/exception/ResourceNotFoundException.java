343c1eb89d001297db83802e761c4eeba53c6f22
gokarakondaswamy <swamygokarakonda@gmail.com>
2026-08-04 23:20:00 +0530

    Add ResourceNotFoundException to common library

---
+++ common-library/src/main/java/com/retailx/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.retailx.common.exception;
+
+public class ResourceNotFoundException extends  RuntimeException {
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
