33a0214421b1a1882576dff2a0b7dab6e5f78b2b
gokarakondaswamy <swamygokarakonda@gmail.com>
2026-08-07 22:01:32 +0530

    Add common application constants

---
+++ common-library/src/main/java/com/retailx/common/constants/AppConstants.java
@@ -0,0 +1,8 @@
+package com.retailx.common.constants;
+
+public final class AppConstants {
+   private AppConstants(){
+
+   }
+    public static final String PRODUCT_NOT_FOUND = "Product not found";
+}
